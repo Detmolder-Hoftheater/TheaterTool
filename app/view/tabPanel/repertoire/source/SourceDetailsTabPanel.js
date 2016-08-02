803b40ff2a0ee19a6a4acaf2c140dedd811eea42
elena1113 <eschilke@mail.uni-paderborn.de>
2016-08-02 14:31:36 +0200

    graph. layout source view

--- app/view/tabPanel/repertoire/source/SourceDetailsTabPanel.js
+++ app/view/tabPanel/repertoire/source/SourceDetailsTabPanel.js
@@ -4,14 +4,14 @@
  */
 Ext.define('TheaterTool.view.tabPanel.repertoire.source.SourceDetailsTabPanel', {
 	extend: 'Ext.panel.Panel',
-
-layout: {
+	
+	layout: {
 		type: 'vbox',
 		pack: 'start',
 		align: 'stretch'
 	},
-
-bodyPadding: 5,
+	
+	bodyPadding: 10,
 	
 	//minHeight: 300,
 	//resizable: true,
@@ -22,6 +22,7 @@ bodyPadding: 5,
 	border: false,
 	
 	titel: null,
+	rism: null,
 	annot: null,
 	abs: null,
 	language: null,
@@ -35,15 +36,15 @@ bodyPadding: 5,
 	
 	
 	/*setTextInfo: function (infoText) {
-		$('#' + this.id + '-innerCt').html(infoText);
+	$('#' + this.id + '-innerCt').html(infoText);
 	},
-	*/
+	 */
 	
-/*	initComponent: function () {
-		
-		this.items =[];
-		
-		this.callParent();
+	/*	initComponent: function () {
+	
+	this.items =[];
+	
+	this.callParent();
 	},*/
 	
 	
@@ -51,115 +52,128 @@ bodyPadding: 5,
 		
 		var me = this;
 		
+		this.titel = this.createTextField('Einheitstitel');
+		this.rism = this.createTextField('RISM ID');
+		var headpanel_1 = Ext.create('Ext.panel.Panel', {
+			
+			layout: {
+				type: 'table',
+				columns: 2
+			},
+			autoScroll: true,
+			border: false,
+			//height: 300,
+			bodyPadding: 17,
+			items:[
+			this.titel,
+			
+			this.rism]
+		});
+		
+		this.items.add(headpanel_1);
+		
+		var titel_group = Ext.create('Ext.form.FieldSet', {
+			title: '<b style="color:gray;">Titel Varianten</b>',
+			bodyBorder: false,
+			
+			collapsible: true,
+			collapsed: false,
+			items:[]
+		});
+		me.items.add(titel_group);
+		
 		
 		var panel_0 = null;
-
-		panel_0 = Ext.create('Ext.panel.Panel', {					
-					layout: {
-						type: 'table',
-						columns: 2
-						/*tableAttrs: {
-            			style: {
-                			width: '100%'
-            			}
-        				}*/
-					},
-					autoScroll: true,
-					border: false,
-					
-					items:[
-				
-					]
-				});
-				me.items.add(panel_0);
-
-		for (i = 0; i <= 2; i++) {		
-
+		
+		panel_0 = Ext.create('Ext.panel.Panel', {
+			layout: {
+				type: 'table',
+				columns: 2
+				/*tableAttrs: {
+				style: {
+				width: '100%'
+				}
+				}*/
+			},
+			autoScroll: true,
+			border: false,
+			bodyPadding: 10,
+			items:[]
+		});
+		titel_group.add(panel_0);
+		
+		for (i = 0; i <= 2; i++) {
+			
 			me.w_ein_titel = this.createTextField('Einheitstitel');
 			me.w_titel = this.createTextField('Titel');
 			me.w_alt_titel = this.createTextField('Alternativtitel');
 			me.w_unter_titel = this.createTextField('Untertitel');
-
-				panel_10 = Ext.create('Ext.panel.Panel', {					
-					colspan: 1,
-					type: 'hbox',
-					border:false,
-
-					items:[
-				
-					]
-				});
-				panel_0.add(panel_10);
 			
-panel_10.items.add(me.w_ein_titel);
-panel_10.items.add(me.w_titel);
-panel_10.items.add(me.w_alt_titel);
-panel_10.items.add(me.w_unter_titel);
-
+			panel_10 = Ext.create('Ext.panel.Panel', {
+				colspan: 1,
+				type: 'hbox',
+				border: false,
+				style: {
+					borderBottom: '15px solid #FFFFFF'
+				},
+				items:[]
+			});
+			panel_0.add(panel_10);
+			
+			panel_10.items.add(me.w_ein_titel);
+			panel_10.items.add(me.w_titel);
+			panel_10.items.add(me.w_alt_titel);
+			panel_10.items.add(me.w_unter_titel);
 		}
 		
-this.titel = this.createTextArea('Titel (Quelle)');
-		this.language = this.createTextArea('Sprache(n)');
-var panel_00 = Ext.create('Ext.panel.Panel', {
-			
-			layout: {
-				type: 'table',
-				columns: 2
-			/*tableAttrs: {
-            style: {
-                width: '100%'
-            }
-        }*/
-			},
-			autoScroll: true,
+		this.abs = this.createTextField('Entstehung');
+		var ents_panel = Ext.create('Ext.panel.Panel', {
 			border: false,
-			//height: 300,
-			
-			
-			items:[
-			this.titel,
-			this.language]
+			bodyPadding: 17,
+			items:[]
 		});
 		
-		this.items.add(panel_00);
+		ents_panel.add(this.abs);
+		this.items.add(ents_panel);
+		
 		
 		this.annot = this.createTextArea('Bemerkungen');
-		/*this.abs = this.createTextArea('Abschriften');*/
+		
 		
 		this.sign = this.createTextArea('Bibliotheken');
 		this.prov = this.createTextArea('Provienzen');
-
-var panel_01 = Ext.create('Ext.panel.Panel', {
+		
+		var panel_01 = Ext.create('Ext.panel.Panel', {
 			
 			layout: {
 				type: 'table',
 				columns: 2
-			/*tableAttrs: {
-            style: {
-                width: '100%'
-            }
-        }*/
+				/*tableAttrs: {
+				style: {
+				width: '100%'
+				}
+				}*/
 			},
 			autoScroll: true,
 			border: false,
 			//height: 300,
-			
+			bodyPadding: 17,
 			
 			items:[
-			this.sign,
-			this.prov]
+			this.prov,
+			this.sign]
 		});
 		
 		this.items.add(panel_01);
 		
+		var annot_panel = Ext.create('Ext.panel.Panel', {
+			border: false,
+			bodyPadding: 17,
+			items:[]
+		});
 		
-		
-		//this.items.add(this.titel);
-		this.items.add(this.annot);
-		//this.items.add(this.abs);
-		//this.items.add(this.language);
-		//this.items.add(this.sign);
-		//this.items.add(this.prov);
+		annot_panel.add(this.annot);
+		this.items.add(annot_panel);
 		
 		//this.titel.setValue(value);
 	},
@@ -170,13 +184,14 @@ var panel_01 = Ext.create('Ext.panel.Panel', {
 		var textArea = Ext.create('Ext.form.field.TextArea', {
 			name: fieldName,
 			fieldLabel: fieldName,
-
-//width: 235,
+			
+			//width: 235,
 			readOnly: true,
-			anchor    : '100%',
-style: {
-                width: '100%'
-            }
+			anchor: '100%',
+			style: {
+				width: '100%',
+				borderLeft: '5px solid #FFFFFF'
+			}
 		});
 		
 		return textArea;
@@ -187,12 +202,14 @@ style: {
 		var textArea = Ext.create('Ext.form.field.Text', {
 			name: fieldName,
 			readOnly: true,
-style: {
-                width: '100%'
-            },
-//width: 235,
+			
+			style: {
+				width: '100%',
+				borderLeft: '5px solid #FFFFFF'
+			},
+			//width: 235,
 			fieldLabel: fieldName,
-			anchor    : '100%'
+			anchor: '100%'
 		});
 		
 		return textArea;
