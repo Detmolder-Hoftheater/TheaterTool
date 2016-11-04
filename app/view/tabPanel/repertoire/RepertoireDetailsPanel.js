c521a03cf5c1bb23f91adc36961efdb969fab986
elena1113 <eschilke@mail.uni-paderborn.de>
2016-11-04 10:21:48 +0100

    move stores from application to store folder

--- app/view/tabPanel/repertoire/RepertoireDetailsPanel.js
+++ app/view/tabPanel/repertoire/RepertoireDetailsPanel.js
@@ -1,38 +1,42 @@
-/**
- * Creates class TheaterTool.view.tabPanel.CEPanel that extend from Ext.panel.Panel.
- * @class
- */
 Ext.define('TheaterTool.view.tabPanel.repertoire.RepertoireDetailsPanel', {
-	extend: 'Ext.panel.Panel',
+    extend: 'Ext.panel.Panel',
     xtype: 'layout-border',
-    requires: [
-        'Ext.layout.container.Border'
-    ],
+    requires:['Ext.layout.container.Border'],
     layout: 'border',
+    flex: 1,
     bodyBorder: false,
-border: false,
- style: {
-      borderRight: '7px solid white',
-      borderLeft: '7px solid white',
-      borderTop: '7px solid white',
-     borderBottom: '7px solid white'
+    border: false,
+    style: {
+        borderRight: '7px solid white',
+        borderLeft: '7px solid white',
+        borderTop: '7px solid white',
+        borderBottom: '7px solid white'
     },
     
-   defaults: {
-		autoScroll: true,
-		split: true
-	},
-	
-	
-	initComponent: function () {
-	
-
-				this.navTreeStore.sort('name');
-	
-	
-	
-    this.items = [
-    ]
-    	this.callParent();
-	}
+    defaults: {
+        autoScroll: true,
+        split: true
+    },
+    
+    selection: null,
+    
+    initComponent: function () {
+        
+        var navTree = new TheaterTool.view.tabPanel.repertoire.RepertoireNavigationTree();
+        
+        var navTreeStore = new TheaterTool.store.work.Works();
+        navTreeStore.getProxy().extraParams.workName = this.selection;
+        navTreeStore.load();
+        navTree.getView().bindStore(navTreeStore);
+        navTreeStore.sort('name');
+        
+        var repertoirePanel = new TheaterTool.view.tabPanel.repertoire.RepertoirePanel();       
+        navTree.setRepertoirePanel(repertoirePanel);
+        
+        this.items =[
+            navTree,
+            repertoirePanel
+        ]
+        this.callParent();
+    }
 });
