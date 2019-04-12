f44fbac786dbfc141ff878fd841e426416e94481
eschilke <eschilke@mail.uni-paderborn.de>
2019-04-12 07:40:49 +0200

    move this to theater-data

--- app/view/tabPanel/media/MediaPanelInTab.js
+++ app/view/tabPanel/media/MediaPanelInTab.js
@@ -1,25 +1,19 @@
 Ext.define('TheaterTool.view.tabPanel.media.MediaPanelInTab', {
-extend: 'Ext.panel.Panel',
-
-    requires: [
-        'Ext.toolbar.TextItem',
-        'Ext.view.View',
-        'Ext.ux.DataView.Animated'
-    ],
-
-    xtype: 'dataview-multisort',
-    layout: 'fit', 
+    extend: 'Ext.panel.Panel',
     
-   /* layout: {
-        type: 'vbox',
-        pack: 'start',
-        align: 'stretch'
-    },*/
+    requires:[
+    'Ext.toolbar.TextItem',
+    'Ext.view.View',
+    'Ext.ux.DataView.Animated'],
+    
+    xtype: 'dataview-multisort',
+    layout: 'fit',
+   
     autoScroll: true,
     border: false,
     bodyBorder: false,
     bodyPadding: 10,
-   
+    
     
     flex: 1,
     dbkey: null,
@@ -29,49 +23,27 @@ extend: 'Ext.panel.Panel',
     initComponent: function () {
         
         var me = this;
-       
+        
         me.items = {
-           /* xtype: 'dataview',
-            inline:true,*/
-           xtype: 'dataview',
-           
-                //height: '100%',
-                //flex:1,
-               // styleHtmlContent: true,
-               //width: '100%',
-               // centered:true,
-                /* layout: {
-                    type: 'fit'
-                },
-                inline: {
-                    wrap: true
-                },*/
-            itemTpl: [
-                //'<tpl for=".">',
-                    '<div  class="arHeadline">',
-                   /* '<div class="dataview-multisort-item">', */                     
-                        '<a href="{url}">',
-                        '<div>{name}</div>',
-                        '<p></p>',
-                       '<img width="40%" height="auto" src="resources/images/media/{thumb}" />',
-                      // <img src="resources/images/media/{thumb}" />',                       
-                        '</a>',
-                        '<p></p>',
-                    '</div>'
-                //'</tpl>'
+            xtype: 'dataview',
+            itemTpl:[
+            '<div  class="arHeadline">',
+            '<a href="{url}">',
+            '<div>{name}</div>',
+            '<p></p>',
+            '<img width="40%" height="auto" src="/exist/rest/db/apps/theater-data/media/{thumb}" />',
+            '</a>',
+            '<p></p>',
+            '</div>'
             ],
             itemCls: 'dataview-item',
-           /* plugins: {
-                xclass: 'Ext.ux.DataView.Animated'
-            },*/
             itemSelector: 'div.dataview-multisort-item',
             store: Ext.create('Ext.data.Store', {
                 autoLoad: true,
-                //sortOnLoad: true,
-                fields: ['name', 'thumb', 'url', 'type'],
+                fields:[ 'name', 'thumb', 'url', 'type'],
                 proxy: {
                     type: 'ajax',
-                    url : 'resources/data/sencha-touch-examples.json',
+                    url: '/exist/rest/db/apps/theater-data/data/media.json',
                     reader: {
                         type: 'json',
                         rootProperty: ''
@@ -79,33 +51,8 @@ extend: 'Ext.panel.Panel',
                 }
             })
         };
-
-        //this.callParent(arguments);
-        
-        /*var test = new Array();
-        for(i = 0; i < 38; i++){
-        test[i] = {
-        icon: 'resources/images/media/'+(i+1)+'.jpg'
-            }
-           
-        }
-        me.items =test;*/
-        
-       
-       
-        
-        
-        
-       
-       /* me.listeners = {
-            click: function () {
-                     console.log('da');       
-                            }
-                    
-            }*/
         
         me.callParent();
-       // me.updateStoreSorters();
+
     }
-    
 });
