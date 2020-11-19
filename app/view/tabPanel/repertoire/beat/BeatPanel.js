6ce7a394a33e573552eec7718f469ce9516a960e
eschilke <eschilke@mail.uni-paderborn.de>
2020-11-19 08:39:51 +0100

    change Edirom path

--- app/view/tabPanel/repertoire/beat/BeatPanel.js
+++ app/view/tabPanel/repertoire/beat/BeatPanel.js
@@ -37,41 +37,45 @@ Ext.define('TheaterTool.view.tabPanel.repertoire.beat.BeatPanel', {
         var me = this;
         
         var selFolder = null;
+        var editFile = null;
         console.log(me.selectedWork);
         if (me.selectedWork === 'H020149') {
             selFolder = 'aschenbroedel';
+            editFile = 'aschenbroedel/edition-HT_Isouard.xml';
         } else if (me.selectedWork === 'H020263') {
             selFolder = 'bettelstudent';
+            editFile = 'bettelstudent/edition-HT_Bettelstudent.xml';
         } else if (me.selectedWork === 'H020048') {
             selFolder = 'desTeufelsAnteil';
+            editFile = 'desTeufelsAnteil/edition-Auber_DesTeufelsAnteil.xml';
         } else if (me.selectedWork === 'H020076') {
             selFolder = 'unbekannte';
+            editFile = 'unbekannte/edition.xml';
         }else if (me.selectedWork === 'H020166') {
             selFolder = 'edition-HT_Joseph';
+             editFile = 'joseph/edition_Joseph_DT.xml';
         }else if (me.selectedWork === 'H020224') {
             selFolder = 'yelvaLortz';
+            editFile = 'yelvaLortzing/edition_Yelva_Lortzing.xml';
         }else if (me.selectedWork === 'H021013') {
             selFolder = 'yelvaReissiger';
+            editFile = 'yelvaReissiger/edition_Yelva_Reissiger.xml';
         }else if (me.selectedWork === 'H020261') {
             selFolder = 'edition_Der_Mueller';
+            editFile = 'muellerBalett/edition_MuellerBalett.xml';
+        }else if (me.selectedWork === 'H020090') {
+            selFolder = 'kapellmeister';
+            editFile = 'derKapellmeister/edition_DerKapellmeister.xml';
+        }else if (me.sourceID === 'H220246') {
+            selFolder = 'edition_Dresden_Joseph';
+            editFile = 'josephDresden/edition_Joseph_Dresden.xml';
         }
         
         var folderForEO = selFolder + '/';
         
         if (me.selectedWork === 'H020090' || me.sourceID === 'H220246') {
+           
             me.tbar = {
-                style: {
-                    background: 'white'
-                },
-                border: false,
-                fixed: true,
-                height: 30,
-                html: '<i>Die Faksimiles werden noch nicht angezeigt. Die Seite ist noch in Bearbeitung.</i>',
-                style: 'display:block; padding:5px 0px 5px 10px; background: white;'
-            };
-        } else {
-            if(me.selectedWork === 'H020261' || me.selectedWork === 'H021013' || me.selectedWork === 'H020224' || me.selectedWork === 'H020166'){
-                me.tbar = {
                 style: {
                     background: 'white'
                 },
@@ -85,13 +89,21 @@ Ext.define('TheaterTool.view.tabPanel.repertoire.beat.BeatPanel', {
                     border: false,
                     fixed: true,
                     height: 30,
-                    html: '<i>Edirom Online wird noch nicht angezeigt. Die Seite ist noch in Bearbeitung.</i>',
+                    html: '<i>Die Faksimiles werden noch nicht angezeigt. Die Seite ist noch in Bearbeitung.</i>',
                     style: 'display:block; padding:5px 0px 5px 10px; background: white;'
+                },
+                {
+                    xtype: 'component',
+                    margin: '0 0 0 13',
+                    autoEl: {
+                        tag: 'a',                       
+                        href: 'https://edirom.hoftheater-detmold.de/index.html?edition=xmldb:exist:///db/apps/theater-data/vertaktung/' + editFile,
+                        html: 'Zur Erschließung mit Edirom Online',
+                        target: "_blank"
+                    }
                 }]
             };
-                
-            }
-        
+           } 
         else{
             me.tbar = {
                 style: {
@@ -100,29 +112,21 @@ Ext.define('TheaterTool.view.tabPanel.repertoire.beat.BeatPanel', {
                 border: false,
                 fixed: true,
                 height: 25,
-                items:[ /*{
-                    style: {
-                        background: 'white'
-                    },
-                    border: false,
-                    fixed: true,
-                    height: 30,
-                    html: '<i>Edirom Online ist wegen des Datenbankumzugs noch nicht verfügbar, ist aber in Kürze wieder erreichbar.</i>',
-                    style: 'display:block; padding:5px 0px 5px 10px; background: white;'
-                },*/ {
+                items:[ 
+                {
                     xtype: 'component',
                     margin: '0 0 0 13',
                     autoEl: {
                         tag: 'a',                       
-                        href: 'https://edirom.hoftheater-detmold.de/' + folderForEO,
+                        href: 'https://edirom.hoftheater-detmold.de/index.html?edition=xmldb:exist:///db/apps/theater-data/vertaktung/' + editFile,
                         html: 'Zur Erschließung mit Edirom Online',
                         target: "_blank"
                     }
                 }]
             };
-            }
+      
         }
-        
+      
         me.detailSection = new TheaterTool.view.tabPanel.repertoire.beat.FacsimileView({
             selectedWork: me.selectedWork, xmlId: me.xmlId, sourceID: me.sourceID
         });
